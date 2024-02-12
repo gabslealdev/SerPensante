dd0d6fc4fba528c5d5621e82ab377fddfee3460e
Gabriel Leal - PC <gabbrielsleal@gmail.com>
2024-02-12 15:23:04 -0300

    Organizando program

--- SerPensanteApi/Program.cs
+++ SerPensanteApi/Program.cs
@@ -7,35 +7,54 @@ using SerPensanteApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+ConfigureAuthentication(builder);
 
-builder.Services.AddAuthentication(x => {
-    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(x => {
-    x.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ValidateIssuer = false,
-        ValidateAudience = false,
-    };
-});
+ConfigureMvc(builder);
 
-
-
-builder.Services.AddControllers().ConfigureApiBehaviorOptions(options =>
-{
-    options.SuppressModelStateInvalidFilter = true;
-});
-
-builder.Services.AddDbContext<SpenDataContext>();
-builder.Services.AddTransient<TokenService>();
+ConfigureService(builder);
 
 var app = builder.Build();
 
 app.UseAuthentication();
+
 app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
+
+void ConfigureAuthentication(WebApplicationBuilder buider)
+{
+    var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+
+    builder.Services.AddAuthentication(x =>
+    {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }).AddJwtBearer(x =>
+    {
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+        };
+    });
+}
+
+void ConfigureMvc(WebApplicationBuilder builder)
+{
+    builder.Services.AddControllers().ConfigureApiBehaviorOptions(options =>
+        {
+            options.SuppressModelStateInvalidFilter = true;
+        });
+}
+
+void ConfigureService(WebApplicationBuilder builder)
+{
+    builder.Services.AddDbContext<SpenDataContext>();
+    builder.Services.AddTransient<TokenService>();
+}
+
+
