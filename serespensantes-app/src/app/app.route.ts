1aac87b4e33115e6b0a6757a10be75052f6895a4
Gabriel Leal - PC <gabbrielsleal@gmail.com>
2024-05-13 16:01:33 -0300

    prototipo de UI para matricula de aluno

--- serespensantes-app/src/app/app.route.ts
+++ serespensantes-app/src/app/app.route.ts
@@ -2,8 +2,11 @@
 import { Routes } from "@angular/router"
 import { SearchComponent } from "./search/search.component"
 import { SubjectComponent } from "./subject/subject.component"
+import { CoursesComponent } from "./courses/courses.component"
 
 export const ROUTES: Routes = [
-    {path: '', component: SearchComponent}
+    {path: '', component: SearchComponent},
+    {path: '', component: SubjectComponent},
+    {path: '/subject/science:string', component: CoursesComponent}
 ]
     
---
+++ serespensantes-app/src/app/courses/courses.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'srp-courses',
+  standalone: true,
+  imports: [],
+  templateUrl: './courses.component.html',
+  styleUrl: './courses.component.css'
+})
+export class CoursesComponent {
+
+}
